87c30f9501480dc95990041bcb4a73a76fd39f9c
aditya thakur <adityathakur1677@gmail.com>
2025-03-30 09:44:48 +0530

    Delete my-app directory
    Delete

--- my-app/src/Components/Context.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-export const MyContext=createContext({
-data:[], //for array
-addExpense:()=>{}, //call that array
-setData:()=>{} ,//delete ke liye bnaya
-setEdit:()=>{}
-});
