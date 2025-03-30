7b44387ee2905604c98392e3442d60537d9df777
aditya thakur <adityathakur1677@gmail.com>
2025-03-30 09:43:28 +0530

    Delete day1 directory
    delete files

--- day1/src/Component/Firstcomp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Firstcomp( props) {
-  return (
-    <div style={props.style}>
-      <h2>hello i'm aditya</h2>
-    </div>
-  )
-}
--- day1/src/Component1/Firstchild.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function firstchild(props) {
-  return (
-    <div style={props.style}>
-      <h2>{props.name+props.age}</h2>
-    </div>
-  )
-}
